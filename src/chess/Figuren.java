acb0d8a1b7fc2feaf475410f69276ee9782c7f88
frederik <frederik@mac.fritz.box>
2026-02-17 13:05:55 +0100

    Initial commit

---
+++ src/chess/Figuren.java
@@ -0,0 +1,6 @@
+package chess;
+
+public enum Figuren 
+{
+	SPRINGER,TURM,KOENIG,DAME,BAUER,LAEUFER
+}
---
+++ src/chess/Schachbrett.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Schachbrett {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
